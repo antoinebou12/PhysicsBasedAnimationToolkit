5b38848bd0971a3aa910fa3fcfcf2628bb532422
Antoine Boucher <antoine.bou13@gmail.com>
2024-09-13 19:57:44 -0400

    Create PGSSolver.h

---
+++ source/pbat/math/linalg/PGSSolver.h
@@ -0,0 +1,48 @@
+#ifndef PBAT_MATH_LINALG_PGS_SOLVER_H
+#define PBAT_MATH_LINALG_PGS_SOLVER_H
+
+#include "pbat/Aliases.h"
+#include "PhysicsBasedAnimationToolkitExport.h"
+
+namespace pbat {
+namespace math {
+namespace linalg {
+
+/**
+ * @brief Projected Gauss-Seidel (PGS) solver for solving linear complementary problems (LCPs).
+ */
+class PGSSolver
+{
+  public:
+    /**
+     * @brief Default constructor for PGSSolver.
+     */
+    PBAT_API PGSSolver();
+
+    /**
+     * @brief Solves the system using the PGS method.
+     * 
+     * @param A The system matrix (sparse).
+     * @param b The right-hand side vector.
+     * @param x The solution vector.
+     * @param maxIterations The maximum number of iterations.
+     * @param tolerance The tolerance level for convergence.
+     * @return true If the solution converged.
+     * @return false If the solution did not converge within the maximum number of iterations.
+     */
+    PBAT_API bool Solve(
+        const Eigen::SparseMatrix<double>& A,
+        const Eigen::VectorXd& b,
+        Eigen::VectorXd& x,
+        int maxIterations = 1000,
+        double tolerance = 1e-5);
+
+  private:
+    // Additional private helper functions for PGS can go here.
+};
+
+} // namespace linalg
+} // namespace math
+} // namespace pbat
+
+#endif // PBAT_MATH_LINALG_PGS_SOLVER_H
